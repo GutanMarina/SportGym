36c4cfce14eb02a7a7a89cd39b66e8f5b772b926
Alexandra-Paula <sandamanea05@gmail.com>
2025-05-20 16:23:54 +0300

    Add UserAPI in BL Core

---
+++ eStrong.BusinessLogic/Core/UserAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eStrong.BusinessLogic.DBModel;
+using eStrong.Domain.Entities.User;
+using eStrong.Domain.Model.User;
+using System.Linq;
+using System.Data.Entity;
+namespace eStrong.BusinessLogic.Core
+{//implementarea metodei
+    public class UserAPI
+    {
+        public string UserAuthLogicAction(UserLoginDTO data)
+        {
+            return "token key";
+        }
+        public void UserRegisterAction(UDbTable data)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                if (db.UDbTable.Any(u => u.Email == data.Email || u.Username == data.Username))
+                {
+                    throw new Exception("Acest utilizator există deja!");
+                }
+
+                db.UDbTable.Add(data);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
