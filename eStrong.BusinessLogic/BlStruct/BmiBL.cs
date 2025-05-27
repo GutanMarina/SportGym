54dc7a088503bf40d0ceb59a013bdd4022606ab9
valeriacucoara <valericacucoara@gmail.com>
2025-05-27 15:23:01 +0300

    Add BmiBL

---
+++ eStrong.BusinessLogic/BlStruct/BmiBL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eStrong.BusinessLogic.Core;
+using eStrong.BusinessLogic.Interfaces;
+using eStrong.Domain.Model.BMI;
+
+namespace eStrong.BusinessLogic.BlStruct
+{
+    public class BmiBL : UserAPI, IBmi
+    {
+        public decimal CalculateBMI(BMICalculate bmi)
+        {
+            return CalculateBMIAction(bmi);
+        }
+        public bool SaveBmi(BMICalculate data, int userId)
+        {
+            return SaveBmiAction(data, userId);
+        }
+        public decimal BmiResult(int userId)
+        {
+            return BmiResultAction(userId);
+        }
+    }
+}
