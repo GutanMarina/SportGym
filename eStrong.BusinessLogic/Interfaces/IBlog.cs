3a3a78aa1a45dcc90f3b91c17b99c2f6d6361f3f
Gutan Marina <gutanmarina123@gmail.com>
2025-05-27 14:28:03 +0300

    add interface for blog

---
+++ eStrong.BusinessLogic/Interfaces/IBlog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using eStrong.Domain.Entities;
+
+namespace eStrong.BusinessLogic.Interfaces
+{
+    public interface IBlog
+    {
+        List<BlogDbTable> GetAllBlogs();
+        BlogDataDbTable GetBlogById(int blogId);
+
+        List<BlogDbTable> GetBlogsBySearchQuery(string search, List<BlogDbTable> allBlogs);
+
+    }
+}
