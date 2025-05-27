4fd9d60a4b21ef146e51a1101de6c502fe650ddf
valeriacucoara <valericacucoara@gmail.com>
2025-05-27 15:27:36 +0300

    Add entities for functionalites BMI calculate

---
+++ eStrong.Domain/Model/BMI/BMICalculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStrong.Domain.Model.BMI
+{
+      public class BMICalculate
+    {
+        public decimal Height { get; set; }
+        public decimal Weight { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public decimal BMI { get; set; }
+
+    }
+}
