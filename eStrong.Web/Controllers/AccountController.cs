c21d8e2752d69948cf8493dd21a312228aa4e6b7
valeriacucoara <valericacucoara@gmail.com>
2025-05-27 15:29:18 +0300

    Add AccountController

---
+++ eStrong.Web/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eStrong.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            if (Request.Cookies["X-KEY"] != null)
+            {
+                var cookie = new HttpCookie("X-KEY")
+                {
+                    Expires = DateTime.Now.AddDays(-1)
+                };
+                Response.Cookies.Add(cookie);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
