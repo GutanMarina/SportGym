d7c3baa9396c5147d100ba0128b6fe8ef14a6aa4
valeriacucoara <valericacucoara@gmail.com>
2025-05-27 15:26:10 +0300

    Add entities for multiple functionalities

---
+++ eStrong.Domain/Entities/Session.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eStrong.Domain.Entities
+{
+    [Table("Sessions")]
+    public class Session
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SessionId { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "CookieString")]
+        public string CookieString { get; set; }
+
+        [Required]
+        [Display(Name = "ExpireTime")]
+        public DateTime ExpireTime { get; set; }
+    }
+}
