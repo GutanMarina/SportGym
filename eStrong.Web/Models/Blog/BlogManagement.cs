255ee1de1c9f213e3aa321ff0d5d7daef9b488c7
Alexandra-Paula <sandamanea05@gmail.com>
2025-05-28 21:37:05 +0300

    Adding BlogManagement

---
+++ eStrong.Web/Models/Blog/BlogManagement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eStrong.Web.Models.Blog
+{
+    public class BlogManagement
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+
+    }
+}
