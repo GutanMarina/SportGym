3f414d8859d07790eaf764ebe7659398e7c11ec9
Gutan Marina <gutanmarina123@gmail.com>
2025-06-02 12:02:44 +0300

    add filtres for admin activity

---
+++ eStrong.Web/Filtres/AdminAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using eStrong.Domain.Enums;
+using eStrong.Domain.Model.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eStrong.Web.Filtres
+{
+    public class AdminAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var user = httpContext.Session["User"] as UserMinimal;
+            if (user == null)
+            {
+                return false;
+            }
+            return user.Level.ToString() == URole.Admin.ToString();
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Login");
+        }
+    }
+}
