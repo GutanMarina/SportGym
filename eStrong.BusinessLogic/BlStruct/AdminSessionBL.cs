10a24b69f41d6fd5ca1a565446498c43700fcbee
Gutan Marina <gutanmarina123@gmail.com>
2025-06-02 12:01:55 +0300

    created bl and interface for admin logic

---
+++ eStrong.BusinessLogic/BlStruct/AdminSessionBL.cs
@@ -0,0 +1,28 @@
+﻿using eStrong.BusinessLogic.Core;
+using eStrong.BusinessLogic.Interfaces;
+using eStrong.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStrong.BusinessLogic.BlStruct
+{
+   public class AdminSessionBL: AdminApi, IAdminSession 
+    {
+        public bool CreateBlog(BlogDbTable blog)
+        {
+            return CreateBlogAction(blog);
+        }
+        public bool UpdateBlog(BlogDbTable blog)
+        {
+            return UpdateBlogAction(blog);
+        }
+        public bool DeleteBlog(int id)
+        {
+            return DeleteBlogAction(id);
+        }
+
+    }
+}
